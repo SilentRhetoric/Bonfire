830f5909282e0d6807e0a5a706993f6fe265fb19
SilentRhetoric <bwhippo@gmail.com>
2023-11-03 20:55:31 -0400

    Proposal site

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Bonfire
-An interface for burning Algorand ASAs in a permissionless, verifiable, and ARC-54 standard way.
+
+Bonfire is a web interface for burning Algorand ASAs in a permissionless, verifiable, and ARC-54 standard way.
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}"],
+  darkMode: "class",
+  daisyui: {
+    themes: ["luxury"],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+}
+
+export default config
