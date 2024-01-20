24a503e92ba73233c8e136d7408031c54a0da5e5
SilentRhetoric <bwhippo@gmail.com>
2024-01-20 10:51:44 -0500

    Update deps, app version

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -96,6 +96,8 @@ export default function About() {
         recovered unless they have clawback enabled. This software is provided under MIT license,
         and the source code is available through the GitHub link in the page footer.
       </p>
+      <h3>App Info</h3>
+      <p>Version 0.9.1</p>
     </div>
   )
 }
